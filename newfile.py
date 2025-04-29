a185b83f33dc9be3967d27f9455eb8702bb71f5a
matthieubuy <matthieubuy@gojob.com>
2025-04-29 15:19:04 +0200

    29-04-2025 message

---
+++ newfile.py
@@ -0,0 +1,11 @@
+print("hello-world")
+
+def square(list) -> list :
+    list_squared = []
+    for element in list :
+        list_squared.append(element**2)
+    return list_squared
+
+list1 = [1,2,3,4,5,6]
+
+print(square(list1))
